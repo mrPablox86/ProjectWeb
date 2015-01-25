cb4411d8000877be0bb7517ce74c1735a16f872c
x86 <pavellabzin@gmail.com>
2015-01-25 23:33:54 +0300

    добавлен класс primery.cs

---
+++ ProjectWeb/Web/primery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    class Primery
+    {
+        int interpreter = 890;
+
+        public Primery()
+        {
+            interpreter = 32;
+        }
+    }
+}
